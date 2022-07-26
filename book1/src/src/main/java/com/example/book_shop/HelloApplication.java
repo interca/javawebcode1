2f7d34ec64060676d3ac74aaca2b0e1317d6707c
hongsheng <1634340724@qq.com>
2022-07-26 18:05:13 +0800

    master_second

---
+++ book1/src/src/main/java/com/example/book_shop/HelloApplication.java
@@ -0,0 +1,9 @@
+package src.main.java.com.example.book_shop;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ book1/src/src/main/java/com/example/book_shop/HelloResource.java
@@ -0,0 +1,14 @@
+package src.main.java.com.example.book_shop;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
