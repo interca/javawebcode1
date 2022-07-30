a46ad71df332ed64e5dddb59fb359b16aa0df116
hongsheng <1634340724@qq.com>
2022-07-31 01:45:09 +0800

    master_third

--- spring_mybaties_mapper/src/main/java/com/it/domain/UserMapper.java
+++ spring_mybaties_mapper/src/main/java/com/it/domain/UserMapper.java
@@ -5,4 +5,6 @@ import java.util.List;
 public interface UserMapper {
     public List<User> findByCondition(User user);
     public List<User>findByIds(List<Integer>list);
+
+    public  List<User>findAll();
 }
--- spring_mybaties_mapper/src/test/java/com/it/test/MapperTest.java
+++ spring_mybaties_mapper/src/test/java/com/it/test/MapperTest.java
@@ -1,5 +1,7 @@
 package com.it.test;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.it.domain.User;
 import com.it.domain.UserMapper;
 import org.apache.ibatis.io.Resources;
@@ -42,53 +44,28 @@ public class MapperTest {
         List<User> byIds = userMapper.findByIds(list);
         System.out.println(byIds);
     }
-}
+    @Test
+    public  void test3() throws IOException {
+        InputStream inputStream= Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory=new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session=sqlSessionFactory.openSession();
+        UserMapper userMapper=session.getMapper(UserMapper.class);
+        PageHelper.startPage(2,3);
+        List<User>list=userMapper.findAll();
+        for(User user:list){
+            System.out.println(user);
+        }
+        PageInfo<User>pageInfo=new PageInfo<>(list);
+        System.out.println("当前页"+pageInfo.getPageNum());
+        System.out.println("每页显示数量"+pageInfo.getPageSize());
+        System.out.println("总条数"+pageInfo.getTotal());
+        System.out.println("总页数"+pageInfo.getPages());
+        System.out.println("上一页"+pageInfo.getPrePage());
+        System.out.println("下一页"+pageInfo.getNextPage());
+        System.out.println("是否第一页"+pageInfo.isIsFirstPage());
+        System.out.println("是否最后一页"+pageInfo.isIsLastPage());
+        session.close();
 
-class Solution {
-    int[]parent;
-    public int largestComponentSize(int[] nums) {
-        int n=0;
-        for(int i=0;i<nums.length;i++){
-            n=Math.max(n,nums[i]);
-        }
-        parent=new int[n+1];
-        for(int i=0;i<=n;i++){
-            parent[i]=i;
-        }
-        for(int i=0;i<nums.length;i++){
-            int k=nums[i];
-            for(int j=2;j*j<=k;j++){
-                if(k%j==0){
-                    union(j,k);
-                    union(j,k/j);
-                }
-            }
-        }
-        int max=0;
-        int []count=new int[n+1];
-        for(int i=0;i<nums.length;i++){
-            int root=findroot(nums[i]);
-            count[root]++;
-            max=Math.max(max,count[root]);
-        }
-        return max;
-    }
-
-    //联通
-    public void union(int root1,int root2){
-        int k1=findroot(root1);
-        int k2=findroot(root2);
-        if(k1==k2)return;
-        if(k1<k2)
-            parent[k1]=k2;
-        else parent[k2]=k1;
-    }
-
-    //寻找跟，路径压缩
-    public int findroot(int x){
-        if(parent[x]!=x){
-            parent[x]=findroot(parent[x]);
-        }
-        return parent[x];
     }
 }
+
