3d72081e250e83c8375cf49ae8e22b487b7ce69d
hongsheng <1634340724@qq.com>
2022-07-31 04:58:36 +0800

    master_third

---
+++ spring_boot1/src/main/java/com/it/helloapplication.java
@@ -0,0 +1,13 @@
+package com.it;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//引导类，springboot项目的入口
+@SpringBootApplication
+public class helloapplication {
+    public static void main(String[] args) {
+        SpringApplication.run(helloapplication.class,args);
+    }
+}
