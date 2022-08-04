8941b8a6caa976a545ed0d838482f4270e3feca1
hongsheng <1634340724@qq.com>
2022-08-05 06:41:01 +0800

    master_third

---
+++ springboot-swagger/src/main/java/com/it/SpringbootSwaggerApplication.java
@@ -0,0 +1,15 @@
+package com.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class SpringbootSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSwaggerApplication.class, args);
+    }
+
+}
